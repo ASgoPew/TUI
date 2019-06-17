c0ec93f49a0b4a095fb04e4a4899bad7d70574b6
ASgoPew <35419712+ASgoPew@users.noreply.github.com>
2019-06-17 16:21:31 +0300

    вщсгьутефешщт 10

--- TUI/Base/PulseType.cs
+++ TUI/Base/PulseType.cs
@@ -1,11 +1,31 @@
 ﻿namespace TUI.Base
 {
+    /// <summary>
+    /// Type of pulse signal to send to VisualObject(s).
+    /// </summary>
     public enum PulseType
     {
+        /// <summary>
+        /// Object reset signal. Input widgets (like InputLabel, Checkbox, Slider, ...)
+        /// set value to default on this signal.
+        /// </summary>
         Reset = 0,
+        /// <summary>
+        /// This signal is called automatically for whole sub-tree when root of this sub-tree
+        /// changes position/size.
+        /// </summary>
         PositionChanged,
+        /// <summary>
+        /// User defined signal 1
+        /// </summary>
         User1,
+        /// <summary>
+        /// User defined signal 2
+        /// </summary>
         User2,
+        /// <summary>
+        /// User defined signal 3
+        /// </summary>
         User3
     }
 }
