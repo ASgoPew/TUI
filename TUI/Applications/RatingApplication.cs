2f7997e027279a0e9ad91c4e2bdc36734a906b68
ASgoPew <35419712+ASgoPew@users.noreply.github.com>
2021-02-05 20:36:51 +0300

    TSPlayer.Firework(int count)

---
+++ TUI/Applications/RatingApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariaUI.Widgets;
+
+namespace TerrariaUI
+{
+    public class RatingApplication : Application
+    {
+        public RatingApplication(string name)
+            : base($"{name}_rating", Generate)
+        {
+        }
+
+        public static Panel Generate(string name)
+        {
+            return null;
+        }
+    }
+}
---
+++ TUIPlugin/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using TShockAPI;
+using TerrariaUI.Base;
+using Terraria;
+
+namespace TUIPlugin
+{
+    public static class Extensions
+    {
+        public static TSPlayer Player(this Touch touch) =>
+            TShock.Players[touch.PlayerIndex];
+
+        public static void Firework(this TSPlayer player, int count = 1)
+        {
+            int type = 170; // yellow
+            float dx = 16 * 15f;
+            float beginX = 0;
+            if (count > 1)
+                beginX = -(dx / 2f) * (count - 1);
+            float dy = -16 * 4f;
+            for (int i = 0; i < count; i++)
+            {
+                int p = Projectile.NewProjectile(player.TPlayer.position.X + beginX + i * dx,
+                    player.TPlayer.position.Y + dy, 0f, -8f, type, 0, (float)0);
+                Main.projectile[p].Kill();
+            }
+        }
+    }
+}
