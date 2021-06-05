f4e12ca240ff14fe8c23b1905cb6c08eca3bc7ea
ASgoPew <35419712+ASgoPew@users.noreply.github.com>
2021-06-05 23:45:18 +0300

    Started with pagination

---
+++ TUI/Widgets/PagesList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariaUI.Base;
+
+namespace TerrariaUI.Widgets
+{
+    public class PagesList : VisualContainer
+    {
+        #region Data
+
+        public int Page { get; protected set; }
+        public int PagesCount => ChildCount;
+
+        #endregion
+
+        #region Constructor
+
+        public PagesList(int x, int y)
+            : base(x, y, 0, 0, new UIConfiguration() { UseBegin = false }) { }
+
+        #endregion
+        #region GetSizeNative
+
+        protected override (int, int) GetSizeNative()
+        {
+            if (Selected == null && Child.ElementAtOrDefault(0) is VisualObject child)
+                Select(child, false);
+            return (Selected.Width, Selected.Height);
+        }
+
+        #endregion
+
+        #region SelectPage
+
+        public void SelectPage(int index, bool draw = true)
+        {
+            if (index == Page)
+                return;
+
+            VisualObject child = Child.ElementAtOrDefault(index);
+            if (child == null)
+                throw new InvalidOperationException("Trying to select page that doesn't exit");
+
+            Select(Child[index], false);
+            Page = index;
+            Parent.Update().Apply();
+            if (draw)
+                Parent.Draw();
+        }
+
+        #endregion
+        #region NextPage
+
+        public bool NextPage(bool draw)
+        {
+            int page = Page;
+            if (page == PagesCount - 1)
+                return false;
+            SelectPage(page + 1, draw);
+            return true;
+        }
+
+        #endregion
+        #region PreviousPage
+
+        public bool PreviousPage(bool draw)
+        {
+            int page = Page;
+            if (page == 0)
+                return false;
+            SelectPage(page - 1, draw);
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ TUI/Widgets/PaginationMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariaUI.Base;
+using TerrariaUI.Base.Style;
+
+namespace TerrariaUI.Widgets
+{
+    public class PaginationMenu : VisualContainer
+    {
+        #region Data
+
+        protected PagesList Pages;
+
+        #endregion
+
+        #region Constructor
+
+        public PaginationMenu(int x, int y, IEnumerable<string> values, int pageLimit, ButtonStyle style1 = null, ButtonStyle style2 = null,
+                string title = null, LabelStyle titleStyle = null, Input<string> input = null, Action<Menu> titleCallback = null)
+            : base(x, y, 0, 0)
+        {
+            SetupGrid(lines: new ISize[] { new Relative(100), new Absolute(4) }, fillWithEmptyObjects: false);
+            this[0, 0] = Pages = new PagesList(0, 0);
+            int pagesCount = 1;
+            while (values.Count() > 0)
+            {
+                Menu menu = new Menu(0, 0, values.Take(pageLimit), style1, style2, title, titleStyle, input, titleCallback);
+                Pages.Add(menu);
+                values = values.Skip(pageLimit);
+            }
+            Add(new Arrow(0, 0, new ArrowStyle() { Direction = Direction.Left })
+                .SetAlignmentInParent(Alignment.DownLeft, new ExternalIndent() { Left = 1, Down = 1 }));
+            Add(new Arrow(0, 0, new ArrowStyle() { Direction = Direction.Right })
+                .SetAlignmentInParent(Alignment.DownRight, new ExternalIndent() { Right = 1, Down = 1 }));
+            VisualObject footer = this[0, 1] = new VisualObject(0, pageLimit * 4 + (title != null ? 4 : 0), 0, 4);
+            footer.SetupGrid(columns: new ISize[] { new Relative(50), new Relative(50) });
+            footer[0, 0].Callback = (self, touch) =>
+            {
+                if (Pages.PreviousPage(false))
+                {
+                    Update().Apply().Draw();
+                }
+            };
+            footer[1, 0].Callback = (self, touch) =>
+            {
+                if (Pages.NextPage(false))
+                {
+                    Update().Apply().Draw();
+                }
+            };
+        }
+
+        #endregion
+    }
+}
